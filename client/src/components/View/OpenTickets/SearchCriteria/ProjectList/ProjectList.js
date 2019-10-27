046dc833dc507fe314624c9b6d51aff1766e9d59
Jason Brown <jason.brown91@outlook.com>
2019-10-26 18:06:16 -0600

    Started on Search Criteria

--- client/src/components/View/OpenTickets/SearchCriteria/ProjectList/ProjectList.js
+++ client/src/components/View/OpenTickets/SearchCriteria/ProjectList/ProjectList.js
@@ -1,14 +1,18 @@
 import React from "react";
-import Styles from "./ProjectList.scss"
+import Styles from "./ProjectList.module.scss"
 
 const ProjectList = (props) => {
 
+    const {projects} = props;
+
+    const projectNames = projects.map(x => <li key={x}><input type="checkbox" /> {x}</li>)
+
     return (
         <div className={Styles.wrapper}>
-            <h2>Project</h2>
-            <section>
-                <label><input type="checkbox" /> theBrain</label>
-            </section>
+            <h3>Projects:</h3>
+            <ul>
+                {projectNames}
+            </ul>
         </div>
     );
 }
--- client/src/components/View/OpenTickets/SearchCriteria/SearchCriteria.js
+++ client/src/components/View/OpenTickets/SearchCriteria/SearchCriteria.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
 import Styles from "./SearchCriteria.module.scss";
 import ProjectList from "./ProjectList/ProjectList"
+import {storeData} from "../../../../Store"
 
 const SearchCriteria = (props) => {
 
+    const storeDataContext = useContext(storeData);
+    const {openTickets} = storeDataContext.openTicketsHook;
+    const projects = new Set(openTickets.map(x => x.projectName));
+
+
     return (
         <div className={Styles.wrapper}>
-            <ProjectList/>
+            <ProjectList projects={[...projects]}/>
        </div>
     );
 }
