e724e219eacdedfab020828440c2d8d8df8a2217
Jason Brown <jason.brown91@outlook.com>
2019-12-05 17:41:53 -0700

    Built DB model for Sprints

---
+++ models/sprint.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const sprintSchema = new Schema({
+    sprintName: String,
+    start: String,
+    deadline: String,
+    tickets: Array,
+    completed: Boolean,
+    user: String,
+    date: String,
+    id: String,
+   
+})
+
+
+
+module.exports = mongoose.model("Sprint", ticketSchema);
