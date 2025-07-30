3aa57a80d23b505af97c1c5f07c030a344d0a2c4
JasonB101 <jason.brown91@outlook.com>
2025-07-29 18:29:42 -0600

    Is it fixed?

--- routes/ticketsAPI.js
+++ routes/ticketsAPI.js
@@ -3,15 +3,17 @@ const ticketRouter = express.Router();
 const Ticket = require("../models/ticket");
 const verifyUser = require("../sharedFunctions/verifyUser")
 
-ticketRouter.post("/", (req, res, next) => {
+ticketRouter.post("/", async (req, res, next) => {
     const token = req.headers.authorization;
     if (!token) return missingToken(res)();
     
-    const newTicket = new Ticket(req.body)
-    newTicket.save((err, ticket) => {
-        if (err) return res.status(500).send({ success: false, err });
+    try {
+        const newTicket = new Ticket(req.body);
+        const ticket = await newTicket.save();
         return res.status(200).send({ message: "success", ticket: ticket });
-    })
+    } catch (err) {
+        return res.status(500).send({ success: false, err });
+    }
 })
 
 ticketRouter.get("/", async (req, res, next) => {
@@ -21,13 +23,13 @@ ticketRouter.get("/", async (req, res, next) => {
     const [validUser, admin] = await verifyUser(token);
 
     if (validUser) {
-        Ticket.find((err, tickets) => {
-            if (err) {
-                res.status(500)
-                return next(err)
-            }
-            return res.send(tickets)
-        });
+        try {
+            const tickets = await Ticket.find();
+            return res.send(tickets);
+        } catch (err) {
+            res.status(500);
+            return next(err);
+        }
     } else {
         return res.status(403).send({message: "You must be logged in."})
     }
@@ -41,15 +43,14 @@ ticketRouter.delete("/:id", async (req, res, next) => {
     const [validUser, admin] = await verifyUser(token);
     
     if (admin) {
-        let id = req.params.id;
-
-        Ticket.findByIdAndRemove(id, (err, ticket) => {
-            if (err) {
-                console.log(err)
-                return res.status(500).send({ message: "Internal Server Error" })
-            }
+        try {
+            let id = req.params.id;
+            await Ticket.findByIdAndRemove(id);
             return res.status(200).send({ success: true });
-        })
+        } catch (err) {
+            console.log(err);
+            return res.status(500).send({ message: "Internal Server Error" });
+        }
     } else {
         return res.status(401).send({ message: "You are not authorized to remove tickets." })
     }
